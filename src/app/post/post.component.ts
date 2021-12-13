4fe45ea384633f3acd29b11f06f2b2d5d3ea568f
Abdallah <bratosab@gmail.com>
2021-12-13 13:46:49 +0100

    feat(partie 2): Routing Solution 2

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, Subscription, tap } from 'rxjs';
+import {
+  combineLatest,
+  map,
+  Observable,
+  tap,
+} from 'rxjs';
 import { Post } from '../models/post.interface';
 import { PostsService } from '../providers/posts.service';
 
@@ -9,34 +14,65 @@ import { PostsService } from '../providers/posts.service';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
 })
+
+///////////////////
+//   Solution 1  //
+///////////////////
+// export class PostComponent implements OnInit {
+//   posts$: Observable<Post[]> = new Observable<Post[]>();
+//   posts?: Post[];
+//   routeChangeSubscription?: Subscription;
+
+//   post?: Post;
+//   constructor(
+//     private route: ActivatedRoute,
+//     private postsService: PostsService
+//   ) {}
+
+//   ngOnInit(): void {
+//     this.posts$ = this.postsService.getPosts().pipe(
+//       tap((posts) => {
+//         this.posts = posts;
+
+//         this.post = posts.find(
+//           (post) => post.slug === this.route.snapshot.params['slug']
+//         );
+
+//         this.listenForRouteChange()
+//       })
+//     );
+//   }
+
+//   private listenForRouteChange() {
+//     this.routeChangeSubscription = this.route.params.subscribe((params) => {
+//       this.post = this.posts?.find((post) => post.slug === params['slug']);
+//     });
+//   }
+// }
+
+///////////////////
+//   Solution 2  //
+///////////////////
 export class PostComponent implements OnInit {
   posts$: Observable<Post[]> = new Observable<Post[]>();
-  posts?: Post[];
-  routeChangeSubscription?: Subscription;
-
   post?: Post;
+
   constructor(
     private route: ActivatedRoute,
     private postsService: PostsService
   ) {}
 
   ngOnInit(): void {
-    this.posts$ = this.postsService.getPosts().pipe(
-      tap((posts) => {
-        this.posts = posts;
-
-        this.post = posts.find(
-          (post) => post.slug === this.route.snapshot.params['slug']
-        );
-
-        this.listenForRouteChange()
+    this.posts$ = combineLatest([
+      this.postsService.getPosts(),
+      this.route.params,
+    ]).pipe(
+      tap(([posts, params]) => {
+        this.post = posts.find((post) => post.slug === params['slug']);
+      }),
+      map(([posts]) => {
+        return posts;
       })
     );
-  }
-
-  private listenForRouteChange() {
-    this.routeChangeSubscription = this.route.params.subscribe((params) => {
-      this.post = this.posts?.find((post) => post.slug === params['slug']);
-    });
   }
 }
